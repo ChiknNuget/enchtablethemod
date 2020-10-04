743f163d380dc62289ccc6df20b278a6cc3f84d1
Sploopo <69737659+Sploopo@users.noreply.github.com>
2020-10-04 11:30:00 -0400

    Add files via upload

---
+++ Items/HeartCookie.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using ChefClass.Buffs;
+
+namespace ChefClass.Items
+{
+    public class HeartCookie : ChefRecipe
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Recipe: Heart Cookie");
+            Tooltip.SetDefault("Cooks a heart cookie, granting Rapid Healing for 10 seconds\nUsing this prevents you from using recipes for 25 seconds");
+        }
+
+		public override void SetDefaults()
+		{
+			item.width = 20;
+			item.height = 20;
+			item.useTime = 17;
+			item.useAnimation = 17;
+			item.useStyle = 2;
+			item.useTurn = true;
+			item.value = Item.sellPrice(0, 0, 75, 0);
+			item.rare = ItemRarityID.LightRed;
+			item.UseSound = SoundID.Item2;
+		}
+
+        public override bool UseItem(Player player)
+        {
+            player.AddBuff(BuffID.RapidHealing, 600);
+            player.AddBuff(BuffType<Full>(), (60 * 35));
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.PalladiumBar, 8);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+
+    }
+}
---
+++ Items/NoviceChefPan.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+
+namespace ChefClass.Items
+{
+    public class NoviceChefPan : ChefDamageItem
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Novice Chef's Pan");
+		}
+
+		public override void SafeSetDefaults()
+		{
+			item.damage = 10;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.knockBack = 6;
+			item.value = 54;
+			item.rare = ItemRarityID.White;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			item.shootSpeed = 8f;
+			item.shoot = mod.ProjectileType("NoviceChefPanProj");
+		}
+
+		public override bool CanUseItem(Player player)
+		{
+
+			for (int i = 0; i < 1000; ++i)
+			{
+				if (Main.projectile[i].active && Main.projectile[i].owner == Main.myPlayer && Main.projectile[i].type == item.shoot)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Wood, 15);
+			recipe.AddIngredient(ItemID.StoneBlock, 15);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
