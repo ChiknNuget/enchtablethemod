e82bac86203c23e45ba0c60b8e68cb1ef8be2f68
Sploopo <69737659+Sploopo@users.noreply.github.com>
2020-10-04 11:29:14 -0400

    Add files via upload

---
+++ ChefRecipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameInput;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using static Terraria.ModLoader.ModContent;
+using ChefClass.Buffs;
+
+namespace ChefClass
+{
+    public abstract class ChefRecipe : ModItem
+    {
+        public bool isRecipe = true;
+
+        public override bool CanUseItem(Player player)
+        {
+            return !player.HasBuff(BuffType<Full>());
+        }
+    }
+}
