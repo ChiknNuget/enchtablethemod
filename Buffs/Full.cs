60886d5a81251b62e40d904ea608b07159a1b1be
Sploopo <69737659+Sploopo@users.noreply.github.com>
2020-10-04 11:31:59 -0400

    Add files via upload

---
+++ Buffs/Full.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ChefClass.Buffs
+{
+    public class Full : ModBuff
+    {
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Full");
+			Description.SetDefault("You've eaten enough and don't want to cook anything else yet");
+			Main.debuff[Type] = true;
+			Main.buffNoSave[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.GetModPlayer<Chef>().recipeCooldown = true;
+		}
+	}
+}
