578c58ec377585fed8fd56d5a5c92f1b44bd9cac
Sploopo <69737659+Sploopo@users.noreply.github.com>
2020-10-04 11:30:43 -0400

    Add files via upload

---
+++ Chef.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ChefClass
+{
+    public class Chef : ModPlayer
+    {
+        public static Chef ModPlayer(Player player)
+        {
+            return player.GetModPlayer<Chef>();
+        }
+
+        public float chefDamageAdd;
+        public float chefDamageMult = 1f;
+        public float chefKB;
+        public int chefCrit;
+
+        public bool recipeCooldown;
+
+        public override void ResetEffects()
+        {
+            ResetVariables();
+        }
+
+        public override void UpdateDead()
+        {
+            ResetVariables();
+        }
+
+        private void ResetVariables()
+        {
+            chefDamageAdd = 0f;
+            chefDamageMult = 1f;
+            chefKB = 0f;
+            chefCrit = 0;
+
+            recipeCooldown = false;
+        }
+    }
+}
