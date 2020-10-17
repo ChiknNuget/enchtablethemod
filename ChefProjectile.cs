68e65318169e9128dfb7d23921432018292e60df
Sploopo <69737659+Sploopo@users.noreply.github.com>
2020-10-17 19:36:29 -0400

    comedy is hard

---
+++ ChefProjectile.cs
@@ -0,0 +1,11 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace enchtablethemod.Projectiles
+{
+    public abstract class ChefProjectile  : ModProjectile
+    {
+        public bool isChefProj = true;
+    }
+}
---
+++ NoviceChefPanProj.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace enchtablethemod.Projectiles
+{
+    public class NoviceChefPanProj : ChefProjectile
+    {
+        public override void SetDefaults()
+        {
+            projectile.width = 30;
+            projectile.height = 30;
+            projectile.damage = 3;
+            projectile.aiStyle = 3;
+            projectile.friendly = true;
+            projectile.penetrate = 3;
+            projectile.timeLeft = 600;
+            projectile.light = 0.5f;
+            projectile.extraUpdates = 1;
+        }
+    }
+}
