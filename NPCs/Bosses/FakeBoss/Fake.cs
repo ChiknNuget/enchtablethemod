5e9bb731ea21116c0b8e856eba7fe45d7d5f4594
ArnoldPalmergolf <39205784+ArnoldPalmerGolf@users.noreply.github.com>
2020-10-18 11:29:47 -0400

    All of Fake's NPC crap is now stored in here

---
+++ NPCs/Bosses/FakeBoss/Fake.cs
@@ -0,0 +1,269 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace enchtablethemod.NPCs.Bosses.FakeBoss
+{
+    [AutoloadBossHead]
+
+    public class Fake : ModNPC
+    {
+        public int despawn = 0;
+        int PhaseVar { get; set; } = 0;
+        int FunnyCounter { get; set; } = 0;
+        public Vector2 targetPos;
+        public float xAccel = 0;
+        private int movement = -1;
+        public float timer
+        {
+            get => npc.ai[2];
+            set => npc.ai[2] = value;
+        }
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fake");
+            Main.npcFrameCount[npc.type] = 1;
+        }
+
+
+
+        public override void SetDefaults()
+        {
+            npc.width = 68;
+            npc.height = 106;
+
+            npc.boss = true;
+            npc.aiStyle = -1;
+            aiType = -1;
+            npc.npcSlots = 5;
+
+            npc.lifeMax = 1500;
+            npc.damage = 20;
+            npc.defense = 7;
+            npc.knockBackResist = 0f;
+
+            npc.value = Item.buyPrice(gold: 3);
+
+            npc.lavaImmune = true;
+            npc.noTileCollide = true;
+            npc.noGravity = true;
+
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            music = mod.GetSoundSlot(SoundType.Music, "Sounds/Music/DotheMario");
+            musicPriority = MusicPriority.BossHigh;
+        }
+
+        public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
+        {
+            npc.lifeMax = (2000 + (500 * numPlayers));
+            npc.damage = (int)(npc.damage * 1.3f);
+        }
+
+        public override void AI()
+        {
+            despawnTest(npc);
+            executeAttacks();
+
+        }
+
+        private void executeAttacks()
+        {
+            Player player = Main.player[npc.target];
+            npc.TargetClosest(true);
+            Vector2 target = npc.HasPlayerTarget ? player.Center : Main.npc[npc.target].Center;
+            movement = Main.rand.Next(0, 1);
+            npc.netAlways = true;
+            switch (PhaseVar)
+            {
+                case 0:
+                    teleportThing();
+                    timer = 0;
+                    break;
+                case 1:
+                    tp_reset();
+                    minionCrap();
+                    break;
+                case 2:
+                    teleportThingButAwsome();
+                    timer = 0;
+                    break;
+                case 3:
+                    tp_reset();
+                    minionCrap();
+                    break;
+                case 4:
+                    PhaseVar = 0;
+                    FunnyDust();
+                    break;
+            }
+            npc.netUpdate = true;
+        }
+
+        private void teleportThing()
+        {
+            if (FunnyCounter % 2 == 0)
+                targetPos = Main.player[npc.target].Center + new Vector2(Main.rand.Next(300,700), Main.rand.Next(-100,100));
+            else
+            {
+                FunnyDust();
+                npc.position.X -= 1500;
+                FunnyDust();
+                Main.PlaySound(SoundID.Item8, npc.Center);
+            }
+            targetPos = Main.player[npc.target].Center + new Vector2(Main.rand.Next(300, 700), Main.rand.Next(-100, 100));
+            if (npc.Center.Y > targetPos.Y)
+            {
+                npc.velocity.Y -= .2f;
+                if (npc.velocity.Y > 0)
+                    npc.velocity.Y *= .9f;
+            }
+            if (npc.Center.Y < targetPos.Y)
+            {
+                npc.velocity.Y += .2f;
+                if (npc.velocity.Y < 0)
+                    npc.velocity.Y *= .9f;
+            }
+            if (Math.Abs(npc.velocity.Y) > 7)
+                npc.velocity.Y *= .98f;
+            if (npc.Center.X > targetPos.X)
+            {
+                xAccel -= .3f;
+                if (FunnyCounter % 2 == 0)
+                {
+                    FunnyCounter++;
+                }
+            }
+            if (npc.Center.X < targetPos.X)
+            {
+                xAccel += .3f;
+                if (FunnyCounter % 2 != 0)
+                {
+                    FunnyCounter++;
+                }
+            }
+            if (Math.Abs(xAccel) > 7)
+                xAccel *= .98f;
+            npc.velocity.X = xAccel;
+            if (FunnyCounter > 6)
+            {
+                FunnyCounter = 0;
+                PhaseVar++;
+            }
+        }
+
+        private void teleportThingButAwsome()
+        {
+            if (FunnyCounter % 2 == 0)
+                targetPos = Main.player[npc.target].Center + new Vector2(Main.rand.Next(-700, -300), Main.rand.Next(-100, 100));
+            else
+            {
+                FunnyDust();
+                npc.position.X -= -1500;
+                FunnyDust();
+                Main.PlaySound(SoundID.Item8, npc.Center);
+            }
+            targetPos = Main.player[npc.target].Center + new Vector2(Main.rand.Next(-700, -300), Main.rand.Next(-100, 100));
+            if (npc.Center.Y > targetPos.Y)
+            {
+                npc.velocity.Y -= .2f;
+                if (npc.velocity.Y > 0)
+                    npc.velocity.Y *= .9f;
+            }
+            if (npc.Center.Y < targetPos.Y)
+            {
+                npc.velocity.Y += .2f;
+                if (npc.velocity.Y < 0)
+                    npc.velocity.Y *= .9f;
+            }
+            if (Math.Abs(npc.velocity.Y) > 7)
+                npc.velocity.Y *= .98f;
+            if (npc.Center.X < targetPos.X)
+            {
+                xAccel -= -.3f;
+                if (FunnyCounter % 2 == 0)
+                {
+                    FunnyCounter++;
+                }
+            }
+            if (npc.Center.X > targetPos.X)
+            {
+                xAccel += -.3f;
+                if (FunnyCounter % 2 != 0)
+                {
+                    FunnyCounter++;
+                }
+            }
+            if (Math.Abs(xAccel) > 7)
+                xAccel *= .98f;
+            npc.velocity.X = xAccel;
+            if (FunnyCounter > 6)
+            {
+                FunnyCounter = 0;
+                PhaseVar++;
+            }
+        }
+
+        private void minionCrap()
+        {
+            npc.velocity *= 0.9f;
+            if (timer >= 150)
+            {
+                NPC.NewNPC((int)npc.Center.X, (int)npc.Center.Y, NPCType<Phanto>(), npc.whoAmI);
+                timer = 0;
+                PhaseVar++;
+            }
+            timer += 1f;
+        }
+
+        private void tp_reset()
+        {
+            npc.position = Main.player[npc.target].Center + new Vector2(0,-300);
+        }
+
+        private void FunnyDust()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                int dust = Dust.NewDust(npc.position, npc.width, npc.height, 259);
+                Main.dust[dust].velocity *= 6f;
+            }
+        }
+
+        private void despawnTest(NPC npc) // semi-stolen but kind of changed
+        {
+            if (Main.player[npc.target].dead == true || Vector2.Distance(Main.player[npc.target].Center, npc.Center) > 3500)
+            {
+                npc.TargetClosest(true);
+                if (Main.player[npc.target].dead == true || Vector2.Distance(Main.player[npc.target].Center, npc.Center) > 3500)
+                {
+                    if (despawn == 0) despawn++;
+                }
+            }
+            if (despawn > 0)
+            {
+                despawn++;
+                npc.velocity.Y = 10f;
+                npc.noTileCollide = true;
+                if (despawn >= 150) npc.active = false;
+            }
+        }
+
+    }
+}
+
+class FakeFire : ModProjectile
+{
+    public override string Texture => "Terraria/Projectile_" + ProjectileID.CursedFlameHostile;
+    public override void SetDefaults()
+    {
+        projectile.CloneDefaults(ProjectileID.CursedFlameHostile);
+    }
+}
---
+++ NPCs/Bosses/FakeBoss/Phanto.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace enchtablethemod.NPCs.Bosses.FakeBoss
+{
+    public class Phanto : ModNPC
+    {
+        public override void SetDefaults()
+        {
+            	npc.CloneDefaults(NPCID.ShadowFlameApparition);
+		npc.damage = 10;
+            	aiType = NPCID.ShadowFlameApparition;
+        }
+
+        public override void AI()
+        {
+			for (int num368 = 0; num368 < 200; num368++)
+			{
+				if (num368 == npc.whoAmI || !Main.npc[num368].active || Main.npc[num368].type != npc.type)
+				{
+					continue;
+				}
+				Vector2 vector120 = Main.npc[num368].Center - npc.Center;
+				if (!(vector120.Length() < 50f))
+				{
+					continue;
+				}
+				vector120.Normalize();
+				if (vector120.X == 0f && vector120.Y == 0f)
+				{
+					if (num368 > npc.whoAmI)
+					{
+						vector120.X = 0.4f;
+					}
+					else
+					{
+						vector120.X = -0.4f;
+					}
+				}
+				vector120 *= 0.4f;
+				npc.velocity -= vector120;
+				NPC nPC8 = Main.npc[num368];
+				NPC nPC20 = nPC8;
+				nPC20.velocity += vector120;
+			}
+				float num370 = 120f;
+				if (npc.localAI[0] < num370)
+				{
+					if (npc.localAI[0] == 0f)
+					{
+						Main.PlaySound(SoundID.Item8, npc.Center);
+						npc.TargetClosest();
+						if (npc.direction > 0)
+						{
+							npc.velocity.X += 1.1f;
+						}
+						else
+						{
+							npc.velocity.X -= 1.1f;
+						}
+						for (int num371 = 0; num371 < 20; num371++)
+						{
+							Vector2 center27 = npc.Center;
+							center27.Y -= 18f;
+							Vector2 vector121 = new Vector2(Main.rand.Next(-100, 101), Main.rand.Next(-100, 101));
+							vector121.Normalize();
+							vector121 *= (float)Main.rand.Next(0, 100) * 0.1f;
+							center27 += vector121;
+							vector121.Normalize();;
+						}
+					}
+					npc.localAI[0] += 1f;
+					float num373 = 1f - npc.localAI[0] / num370;
+					float num374 = num373 * 20f;
+					for (int num375 = 0; (float)num375 < num374; num375++)
+					{
+						if (Main.rand.Next(5) == 0)
+						{
+							int num376 = Dust.NewDust(npc.position, npc.width, npc.height, 106);
+							Main.dust[num376].alpha = 100;
+							Dust dust32 = Main.dust[num376];
+							Dust dust81 = dust32;
+							dust81.velocity *= 0.3f;
+							dust32 = Main.dust[num376];
+							dust81 = dust32;
+							dust81.velocity += npc.velocity * 0.75f;
+							Main.dust[num376].noGravity = true;
+						}
+					}
+				}
+			}
+		}
+    }
